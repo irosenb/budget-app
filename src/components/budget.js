557f27b19c2cf696f12f1c2b39a020efab5728ab
Isaac Rosenberg <irosenb7@gmail.com>
2023-08-25 16:54:05 -0400

    Add retrieve categories function

--- src/components/budget.js
+++ src/components/budget.js
@@ -1,14 +1,28 @@
 import Airtable from 'airtable';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_KEY}).base(process.env.REACT_APP_AIRTABLE_BASE)
 
 const Budget = () => {
   const [categories, setCategories] = useState()
 
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  const fetchCategories = async () => {
+    const records = await base('Budget').select({
+      fields: ['Name', 'Cost']
+    }).all()
+  
+    console.log(records);
+
+    setCategories(records)
+  } 
+
   return (
     <div>
-      <h1>Budget App</h1>
+      <h1 className='text-3xl font-bold'>Budget App</h1>
     </div>
   )
 }
